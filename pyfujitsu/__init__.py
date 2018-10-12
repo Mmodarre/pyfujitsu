5aa4c91466476f49dcfde7b3d29f785bd93cf8e4
Mehdi Modarressi <memodarr@microsoft.com>
2018-10-12 16:09:23 +1100

    pypi prep

--- pyfujitsu/__init__.py
+++ pyfujitsu/__init__.py
@@ -1,1 +1,2 @@
 """Init file for pyfujitsu"""
+name = "pyfujistsu"
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyfujistsu",
+    version="0.7.0",
+    author="Mehdi Modarressi",
+    author_email="Luckposht@gmail.com",
+    description="Python library to control Fujitsu General Airconditioners on AylaNetworks IoT platform",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mmodarre/pyfujitsu",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

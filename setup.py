42c847df154747afa21a5bb38ddc223913d890d8
xerxes87 <willmertinger@gmail.com>
2019-08-05 13:46:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyfujitsu",
-    version="0.9.1.0",
+    version="0.9.2.1",
     author="Mehdi Modarressi",
     author_email="Luckposht@gmail.com",
     description="Python library to control Fujitsu General Airconditioners on AylaNetworks IoT platform",
